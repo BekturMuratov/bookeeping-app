fabc20dfeb51e7b8a5bec72495f989ca63934f8c
BekturMuratov <Muratov225@gmail.com>
2020-09-05 00:45:35 +0600

    pages and layouts is ready

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,56 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
+    meta:{layout:'main'},
     component: Home
+  },
+  {
+    path: '/login',
+    name:'login',
+    meta:{layout:'empty'},
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/categories',
+    name:'categories',
+    meta:{layout:'main'},
+    component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/detail',
+    name:'detail',
+    meta:{layout:'main'},
+    component: () => import('../views/Detail.vue')
+  },
+  {
+    path: '/history',
+    name:'history',
+    meta:{layout:'main'},
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/planning',
+    name:'planning',
+    meta:{layout:'main'},
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name:'profile',
+    meta:{layout:'main'},
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name:'record',
+    meta:{layout:'main'},
+    component: () => import('../views/Record.vue')
+  },
+  {
+    path: '/register',
+    name:'register',
+    meta:{layout:'empty'},
+    component: () => import('../views/Register.vue')
   }
 ]
 

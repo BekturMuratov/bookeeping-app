651edcf46ade5886dfeab1cd630a5e8133ecff14
BekturMuratov <Muratov225@gmail.com>
2020-09-06 02:27:19 +0600

    registration and login functionalities is ready

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import dateFilter from '@/filters/date.filter.js'
+import MessagePlugin from '@/utils/message.plugin.js'
 import store from './store'
 import 'materialize-css/dist/js/materialize.min'
+
+Vue.use(Vuelidate)
+Vue.use(MessagePlugin)
+
 
 Vue.config.productionTip = false
 Vue.filter('date',dateFilter)
